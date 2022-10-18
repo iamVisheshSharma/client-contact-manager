ceb46e305ccec3279cd606c6ba5f8208ea822489
iamVisheshSharma <61023453+iamVisheshSharma@users.noreply.github.com>
2022-10-19 00:38:40 +0530

    Design changed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
-import "./cardStyle.css";
 
 export default function Card(props) {
   return (
     <div className={`col-xs-12 col-sm-6 col-md-4`}>
-      <div className="card text-center bg-warning w-100 mt-4">
+      <div className="card text-center w-100 mt-4" style={{backgroundColor: '#eee'}}>
         <div className="card-body text-dark">
           <h4 className="card-title font-weight-bold text-monospace">{props.contact.name.toUpperCase()}</h4>
-          <p className="card-text font-weight-normal">{props.contact.address}</p>
-          <p className="card-text">
-            Mobile No - {props.contact.contact_number}
-          </p>
-          {props.contact.alt_contact_number && (
-            <p className="card-text">
-              Mobile No - {props.contact.alt_contact_number}
-            </p>
-          )}
-          <p className="card-text">GSTIN - {props.contact.gstin.toUpperCase()}</p>
+          <p className="card-text font-weight-normal px-32">{props.contact.address}</p>
+          <p className="card-text font-weight-normal">Mobile - {props.contact.contact_number}</p>
+          <h5 className="card-text font-weight-bold">Party Gst - {props.contact.gstin.toUpperCase()}</h5>
           <Link
             to={{
               pathname: `/contact/${props.contact.uuid}`,
